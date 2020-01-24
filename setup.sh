af1e3dd09cfe64fb78f7858fb0e90fb1eaf373ea
ngrande <devlup@outlook.com>
2020-01-25 00:36:28 +0100

    Adjust setup

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ if [ ! -h /usr/bin/rmv-lcd.py ]; then
 	sudo ln -s $(pwd)/rmv-lcd.py /usr/bin/rmv-lcd.py
 fi
 sudo cp ./config.json /etc/rmv-pi/
-sudo cp ./rmv-lcd.service /etc/systemd/user/
-systemctl --user enable rmv-lcd.service
-systemctl --user restart rmv-lcd.service
+sudo cp ./rmv-lcd.service /etc/systemd/system/
+sudo systemctl enable rmv-lcd.service
+sudo systemctl restart rmv-lcd.service
